e248c8d13274a5e2c4cdcbd0dba15cd42f72bd16
michel4alves <michelsantosa@gmail.com>
2023-06-16 11:14:09 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const cord = document.querySelectorAll('.cord')
+cord.forEach((e) => {
+    e.addEventListener('click', () => {
+        const wordBox = e.closest('.word-box')
+        const word = wordBox.querySelector('.word')
+        e.style.top = '-100vh' 
+        word.style.animation = 'cut_cord 2s linear both'
+        word.style.webkitBoxReflect = 'below 0px transparent'
+        word.classList.remove('tied')
+    })
+})
+
+const botãoRetorno = document.querySelector('.back_to_page')
+botãoRetorno.addEventListener('click', () => {
+    window.history.back()
+})
