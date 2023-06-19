097ea1c017043dc6e3afe62cbc43d2fe0a31cb07
Michel4lves <michelsantosa@gmail.com>
2023-06-19 16:54:50 -0300

    add responsive

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ cord.forEach((e) => {
     e.addEventListener('click', () => {
         const wordBox = e.closest('.word-box')
         const word = wordBox.querySelector('.word')
-        e.style.top = '-100vh' 
+        e.style.top = '-400vh' 
         word.style.animation = 'cut_cord 2s linear both'
         word.style.webkitBoxReflect = 'below 0px transparent'
         word.classList.remove('tied')
